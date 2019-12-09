3675f00692caccab84cbe8304359fda2e1b1c2d4
nada96k <nadawe_96@hotmail.com>
2019-12-09 18:14:42 +0300

    add to cart modified

--- stores/cartStore.js
+++ stores/cartStore.js
@@ -2,44 +2,13 @@ import { decorate, observable } from "mobx";
 import { instance } from "./instance";
 
 class CartStore {
-  items = [
-    {
-      id: 1,
-      name: "هوشة",
-      type: "مادري#",
-      owner: {
-        first_name: "",
-        last_name: "",
-        username: "admin"
-      },
-      description: "bla bla bla bla",
-      price: "0.100",
-      img: "http://0.0.0.0:80/media/hosha_2RW2dmI.jpg"
-    },
-    {
-      id: 2,
-      name: "!!سالفة الكتاكيت",
-      type: "مادري#",
-      owner: {
-        first_name: "",
-        last_name: "",
-        username: "admin"
-      },
-      description: "🐥👦🏻🔪",
-      price: "0.050",
-      img:
-        "https://www.thaqfya.com/wp-content/uploads/2018/03/%D8%AA%D9%81%D8%B3%D9%8A%D8%B1-%D8%B1%D8%A4%D9%8A%D8%A9-%D8%A7%D9%84%D9%83%D8%AA%D8%A7%D9%83%D9%8A%D8%AA-%D9%81%D9%8A-%D8%A7%D9%84%D9%85%D9%86%D8%A7%D9%85-%D9%88%D9%85%D8%B9%D9%86%D8%A7%D9%87.png"
-    }
-  ];
+  items = [];
   addItemToCart = async salfa => {
     try {
-      console.log("Salfa", salfa);
       const existingItem = this.items.find(obj => obj.id === salfa.id);
       if (!existingItem) {
-        const res = await instance.post("/api/cart/add/", salfa);
-        const item = res.data;
-        this.items.push(item);
-        // console.log(this.items);
+        const res = await instance.post("/api/cart/add/", { salfa: salfa.id });
+        this.items.push(salfa);
       }
     } catch (err) {
       console.error(err);
